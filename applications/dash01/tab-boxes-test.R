f5498952127776b4af1e73478c7fee49b2af2f37
prei007 <peter.reimann@sydney.edu.au>
2022-11-18 21:55:45 +1100

    Design aspects - work only for Dickes 2019 so far

--- applications/dash01/tab-boxes-test.R
+++ applications/dash01/tab-boxes-test.R
@@ -5,6 +5,7 @@ library(shinydashboard)
 library(shinydashboardPlus)
 library(allegRo)
 library(urltools)
+# library(dplyr)
 
 # Functions 
 
@@ -28,6 +29,7 @@ cat = catalog(service, "coolfutures")
 rep = repository(cat, "compumod")
 addNameSpace(repo = rep, prefix= "edtech", nsURI = "http://coolfutures.net/rdf/2021/edtech#")
 addNameSpace(repo = rep, prefix= "arg", nsURI = "http://www.coolfutures.net/rdf/Design_Conjectures/toulminarg#")
+addNameSpace(repo=rep, prefix="letro", nsURI="http://www.learn-web.com/rdf/2022/letro#")
 
 # get the works 
 
@@ -69,11 +71,11 @@ body <- dashboardBody(
         selectInput(
           inputId = "firstWork", 
           label = "Select first study",
-          choices = c("Dickes2019", "Dickes2016", "option3")),
+          choices = dfworks),
         selectInput(
           inputId = "secondWork", 
           label = "Select second study",
-          choices = c("option1", "Dickes2016", "option3")),
+          choices = dfworks),
         tabBox(
           title = textOutput("firstStudy"),
           # The id lets us use input$tabset1 on the server to find the current tab
@@ -81,11 +83,8 @@ body <- dashboardBody(
           height = "500px",
           tabPanel("Bibliography", htmlOutput('bibText')),
           tabPanel(
-            "Pedagogy",
-            h3("Suspendisse"),
-            "et ornare sapien, non fringilla ligula. ."
-          ),
-          tabPanel("Design", "design goes here"),
+            "Pedagogy", htmlOutput('pedagogyText')),
+          tabPanel("Design", htmlOutput('problemText'), htmlOutput('technologyText')),
           tabPanel(
             "Research",
             "Suspendisse et ornare sapien, non fringilla ligula. ."
@@ -117,8 +116,9 @@ shinyApp(
     body
   ),
   server = function(input, output) {
+    # Name of tabBox
     output$firstStudy <- reactive({ input$firstWork })
-    # The first tab
+    # The bibiolograpic tab
     output$bibText <- renderText({
       query = paste0('select ?title ?date ?doi 
                   {?id a dc:BibliographicResource ; 
@@ -135,4 +135,73 @@ shinyApp(
                "<p>", "<strong>", "DOI: ", "</strong>" , dfout[[3:3]], "</p>"
                )
         })
-  })
+    # The pedagogy tab 
+    output$pedagogyText <- renderText({
+      query = paste0('
+                     select ?Science ?Domain ?Outcome ?Level ?Pedagogy
+                 { ?id a dc:BibliographicResource . 
+                  OPTIONAL {?id edtech:science ?Science}  
+                  OPTIONAL {?id edtech:learningDomain ?Domain} 
+                  OPTIONAL {?id edtech:outcome ?Outcome } 
+                  OPTIONAL {?id edtech:educationalLevel ?Level} 
+                  OPTIONAL {?id edtech:pedagogy ?Pedagogy }
+                  FILTER (?id IN (:', input$firstWork, ')).}'
+      )
+      dfout <- evalQuery(rep,
+                         query = query, returnType = "dataframe",
+                         cleanUp = TRUE, limit = 1000)
+      dfout <- stripOffNS(dfout[["return"]])
+      dfout[[1]] <- fragment(dfout[[1]])
+      dfout[[2]] <- fragment(dfout[[2]])
+      dfout[[3]] <- fragment(dfout[[3]])
+      dfout[[4]] <- fragment(dfout[[4]])
+      dfout[[5]] <- fragment(dfout[[5]])
+      paste0("<p>", "<strong>", "Science domain: ", "</strong> </p>" , 
+             "<p>" , paste0(unique(dfout[[1]]), collapse = ', '), "</p>", 
+             "<p>", "<strong>", "Learning domain: ", "</strong> </p>" , 
+             "<p>" , paste0(unique(dfout[[2]]), collapse = ', '), "</p>",
+             "<p>", "<strong>", "Targeted learning outcome: ", "</strong> </p>" , 
+             "<p>" , paste0(unique(dfout[[3]]), collapse = ', '), "</p>", 
+             "<p>", "<strong>", "Student level: ", "</strong> </p>" , 
+             "<p>" , paste0(unique(dfout[[4]]), collapse = ', '), "</p>", 
+             "<p>", "<strong>", "Pedagogy: ", "</strong> </p>" , 
+             "<p>" , paste0(unique(dfout[[5]]), collapse = ', '), "</p>"
+      )
+    
+    })
+    # The design tab
+    # First the problem(s)
+    output$problemText <- renderText({
+      query = paste0('select  ?ProblemDescription
+            { ?Project a letro:DesignProject ; 
+                 letro:documentedIn ?work; 
+                 letro:motivated_by ?Problem . 
+             ?Problem a letro:Problem ;  
+                      dc:description ?ProblemDescription . 
+            FILTER (?work IN (:', input$firstWork, ')).}')
+      dfout <- evalQuery(rep,
+                         query = query, returnType = "dataframe",
+                         cleanUp = TRUE, limit = 1000)
+      dfout <- stripOffNS(dfout[["return"]])
+ #     dfout[[1]] <- fragment(dfout[[1]])
+      paste0("<p>", "<strong>", "Problem(s) addressed: ", "</strong> </p>" , 
+             "<p>" , paste0(unique(dfout[[1]]), collapse = ', '), "</p>")
+    })
+    # Second the technology
+    output$technologyText <- renderText({
+      query = paste0('select ?Tech
+            { ?id a dc:BibliographicResource . 
+            OPTIONAL {?id edtech:technology ?Tech}  
+            FILTER (?id IN (:', input$firstWork, ')).}'
+      )
+      dfout <- evalQuery(rep,
+                         query = query, returnType = "dataframe",
+                         cleanUp = TRUE, limit = 1000)
+      dfout <- stripOffNS(dfout[["return"]])
+      dfout[[1]] <- fragment(dfout[[1]])
+      paste0("<p>", "<strong>", "Technologies: ", "</strong> </p>" , 
+             "<p>" , paste0(unique(dfout[[1]]), collapse = ', '), "</p>")
+    })
+    # Then the other design aspects 
+    
+  }) 

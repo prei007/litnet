5281825dc4b0c2a9ad2328f48f2ddb1cc94068cc
prei007 <peter.reimann@sydney.edu.au>
2023-07-28 17:28:07 +0200

    first basic UI

--- applications/litgraph/server.R
+++ applications/litgraph/server.R
@@ -1,33 +1,9 @@
+
 server <- function(input, output, session) {
-  scheme_name <- reactive(input$scheme)
-  # compute the dynamic part of the UI
-  output$fillScheme <- renderUI({
-    tagList(
-      # add input fields
-      # To get unique field names, we create <scheme>_<property> IDs:
-      lapply(arg_schemes[[scheme_name()]], function(x)
-        textAreaInput(paste0(
-          names(arg_schemes[scheme_name()]), "_", x
-        ), x)),
-      # and a save button
-      actionButton("b1", "save"))
-    
-  })
-  
-  # Read the field values on button click
-  observeEvent(input$b1, {
-    # re onstruct the field names
-    fields <- lapply(arg_schemes[scheme_name()], function(x)
-      paste0(scheme_name(), "_", x))
-    # read their values. The static input$field syntax is
-    # replaced with the dynamic input[] syntax.
-    # Replace this with addStatement() when AG server part is ready.
-    output$placeholder <- renderText(as.character(lapply(fields[[1]], function(y)
-      paste("field: ",
-            y,
-            " value: ",
-            input[[y]]))))
-  })
+  # Manage hidden panels in the tabset panel
+  observeEvent(input$template, {
+    updateTabsetPanel(inputId = "templates", selected = input$template)
+  }) 
 }
 
 server
--- applications/litgraph/ui.R
+++ applications/litgraph/ui.R
@@ -1,6 +1,10 @@
 # dynamic UI 
 # https://mastering-shiny.org/action-dynamic.html#programming-ui
 
+library(shiny)
+library(shinyjs)
+library(visNetwork)
+library(allegRo)
 
 # setup the schemes and their properties.
 # These will be needed to define fields in the UI. 
@@ -17,46 +21,167 @@
 # This is really to be thought about: Which object propoerties should 
 # be reified, which are just atttributes? 
 
-field_scheme <<- 
-  list(
-    bib_fields = 
-      c("ID", 
-        "Title", 
-        "Creator", 
-        "Date", 
-        "bibliographicCitation"),
-    author_fields = 
-      c("ID", 
-        "family_name",
-        "first_name",
-        "middle_name"),
-    coding_fields =
-      c("ID", 
-        "hasCitingEntity",
-        "hasKnowledgeAssertion_Premise",
-        "hasConclusion"),
-    project_fields =
-      c("ID", 
-        "projectName", 
-        "etc", 
-        "etc2")
+# Reication has definite advantages and the graphing 
+# basis can be constructed with Construct Queries. 
+
+# The interface wfor this app is probably best developed in a mixed 
+# mode, hardcoding the 'tabs' but programming the fields. 
+
+# field_scheme <<- 
+#   list(
+#     bib_fields = 
+#       c("ID", 
+#         "Title", 
+#         "Creator", 
+#         "Date", 
+#         "bibliographicCitation"),
+#     author_fields = 
+#       c("ID", 
+#         "family_name",
+#         "first_name",
+#         "middle_name"),
+#     cito_fields =
+#       c("ID", 
+#         "hasCitingEntity",
+#         "hasCitationCharacterization",
+#         "hasCitedEntity",
+#         ),
+#     project_fields =
+#       c("ID", 
+#         "projectName", 
+#         "etc", 
+#         "etc2")
+#   )
+
+# CiTO ontology
+# https://sparontologies.github.io/cito/current/cito.html
+
+cito_scheme <<- 
+  c("agreesWith",
+    "cites", 
+    "citesAsAuthority",
+    "citesAsDataSource",
+    "citesAsEvidence", 
+    "citesAsMetadataDocument",
+    "citesAsPotentialSolution",
+    "citesAsRecommendedReading",
+    "citesAsRelated",
+    "citesAsSourceDocument",
+    "citesForInformation",
+    "compiles", 
+    "confirms",
+    "containsAssertionFrom",
+    "corrects",
+    "credits", 
+    "critiques",
+    "derides",
+    "describes",
+    "disagreesWith",
+    "discusses",
+    "disputes",
+    "documents",
+    "extends",
+    "givesBackgroundTo",
+    "givesSupportTo",
+    "includesExcerptFrom",
+    "includesQuotationFrom",
+    "likes",
+    "linksTo" ,
+    "parodies",
+    "plagiarizes",
+    "qualifies",
+    "refutes",
+    "repliesTo",
+    "retracts",
+    "reviews",
+    "ridicules",
+    "sharesAuthorWith",
+    "sharesAuthorInstitutionWith",
+    "sharesFundingAgencyWith",
+    "sharesJournalWith", 
+    "sharesPublicationAvenueWith",
+    "speculatesOn",
+    "supports",
+    "updates",
+    "usesConclusionFrom",
+    "usesDataFrom",
+    "usesMethodIn"
   )
 
+# namespaces 
+defaultNS <<- "http://www.example.net/"
+citoNS <<- "http://purl.org/spar/cito/"
+dcNS <<- "http://purl.org/dc/terms/"
+rdfNS <<- "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+rdsNS <<- "http://www.w3.org/2000/01/rdf-schema#"
+foafNS <<- "http://xmlns.com/foaf/0.1/"
+
+ns_list <<- c(defaultNS, citoNS, dcNS, rdfNS, rdsNS, foafNS)
 
 
-defaultNS <<- 'http://www.example.net/'
 
+# Create the tabset 
+template_tabs <- tabsetPanel(
+  id = "templates",
+  type = "hidden",
+  tabPanel("Login", 
+           p("For using the server, you need to provide credentials."),
+           textInput("userName", "User Name:"), 
+           passwordInput("pwd", "Password:"),
+           actionButton("loginButton", "Submit")
+  ),
+  tabPanel("Publication",
+           textInput("pubID", "Publication ID:", value = "",
+                     placeholder = "<FirstAuthor><year>, e.g. Doe2012"),
+           textAreaInput("pubAuthor", "Contributors:", value = "" ,
+                         placeholder = "Author(s)' ID separated by comma"),
+           textAreaInput ("pubTitle", "Title:", value =""),
+           textInput ("pubYear", "Year:", value =""),
+           textInput("pubCite", "Citation:", value="",
+                     placeholder = "DOI or ISBN"),
+           actionButton("savePubButton", "Save new"),
+           actionButton("updatePubButton", "Update") 
+  ),
+  tabPanel("Author",
+           textInput("authorID", "Author ID:", value = "", 
+                     placeholder = "Unique identifier"),
+           textInput("familyName", "Family name:", value = ""),
+           textInput("givenName", "Given name:", value = ""),
+           actionButton("saveAuthorButton", "Save new"), 
+           actionButton("updateAuthorButton", "Update") 
+  ),
+  tabPanel("Citation", 
+           textInput("citingEntity", "Citing paper ID:", value = "", 
+                     placeholder = "e.g :Lemmon2000"), 
+           selectInput("citoType", "Citation type:", cito_scheme) ,
+           textInput ("citedEntity", "Cited paper ID:", value ="", 
+                      placeholder = "e.g. :Hendrix1985"),
+           actionButton("saveCitationButton", "Save new"), 
+           actionButton("updateCitationButton", "Update")
+  )
+)
 
 ui <- fluidPage(
+  useShinyjs(), 
   sidebarLayout(
     sidebarPanel(
-      selectInput("scheme", "Select a scheme", names(field_scheme)),
-      uiOutput("fillScheme"),
+      selectInput("template", "Node template:", 
+                  choices = c("Login", "Publication", "Author", "Citation")
+      ),
+      template_tabs
     ),
     mainPanel(
-      textOutput("placeholder")
+      p("placeholder")
+      # # show user name
+      # textOutput("userName"),
+      # # show a map with the plan from the database.
+      # p(" "),
+      # actionButton("showMapButton",  "show map view "),
+      # p(" "),
+      # visNetworkOutput("Map", width = "1000px", height = "600px")
     )
   )
+  
 )
 
 ui

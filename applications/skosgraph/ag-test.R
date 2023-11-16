4cd39afa3f9710038ff32f1cb79f3b152cdf1a51
prei007 <peter.reimann@sydney.edu.au>
2023-11-16 15:01:10 +0100

    supports the AG package change workflow

---
+++ applications/skosgraph/ag-test.R
@@ -0,0 +1,11 @@
+install.packages("~/Documents/misc/r-stuff/allegRo3", 
+                 repos = NULL,
+                 type = "source")
+
+
+library(allegRo)
+
+# Once changes are done, move the code back to github and do 
+library(devtools)
+install_github ("https://github.com/prei007/allegRo")
+
